7308db95f2f2e13461b712bbc1839f1db76a6247
zhaoyudong <806094224@qq.com>
2025-04-13 13:22:36 +0800

    FlatList横向往返跑马灯效果

--- App.tsx
+++ App.tsx
@@ -1,117 +1,196 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useRef, useEffect } from 'react';
+import {View, FlatList, Animated, Dimensions, Text, StyleSheet, ToastAndroid} from 'react-native';
+import {it} from "@jest/globals";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  // 测试数据
+  const testData = [
+    { id: '1', color: '#FF5733', title: '项目1' ,size: 1,width:Dimensions.get('window').width * 0.5},
+  ];
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const flatListRef = useRef(null);
+  const itemWidth = Dimensions.get('window').width * 0.5; // 每个项目宽度为屏幕宽度的80%
+  const scrollPosition = useRef(0);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  function _genTestData(){
+    let size = testData[0].size;
+    let itemWidth = testData[0].width;
+    let width = size * itemWidth;
+    if(width <= Dimensions.get('window').width){
+      ToastAndroid.show("123",1000)
+      testData.unshift({id: '0',color: '#ffffff',title: '前部分填充',size: 1,width: Dimensions.get('window').width - itemWidth});
+      testData.push({id: '2',color: '#ffffff',title: '',size: 1,width: Dimensions.get('window').width - itemWidth});
+    }else{
+      ToastAndroid.show("456",1000)
+      testData.push({id: '2',color: '#ffffff',title: '',size: 1,width: 0});
+    }
+  }
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  let isStart : boolean = true;
+
+  // 自动滚动效果
+  useEffect(() => {
+    _genTestData();
+    const interval = setInterval(() => {
+      if(isStart){
+        flatListRef.current?.scrollToEnd({ animated: true });
+        // flatListRef.current?.scrollToOffset({
+        //   offset: 2 * itemWidth,
+        //   animated: true,
+        //   duration: 2000 // 最大2秒
+        // });
+        isStart = false;
+      }else{
+        scrollPosition.current = 0;
+        flatListRef.current?.scrollToOffset({
+          offset: scrollPosition.current,
+          animated: true,
+        });
+        // flatListRef.current?.scrollToOffset({
+        //   offset: 0,
+        //   animated: true,
+        //   duration: 2000 // 最大2秒
+        // });
+        isStart = true
+      }
+
+      // scrollPosition.current += itemWidth;
+      //
+      // // 如果滚动到末尾，回到开头
+      // if (scrollPosition.current >= itemWidth * testData.length) {
+      //   scrollPosition.current = 0;
+      // }
+      //
+      // flatListRef.current?.scrollToOffset({
+      //   offset: scrollPosition.current,
+      //   animated: true,
+      // });
+      // flatListRef.current?.scrollToEnd({ animated: true });
+    }, 1000); // 每2秒滚动一次
+
+    return () => clearInterval(interval);
+  }, []);
+
+  let width = 0;
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>自动滚动列表</Text>
+
+        <Animated.FlatList
+            ref={flatListRef}
+            data={testData}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => {
+              const views = [];
+              if(item.id == '1'){
+                for (let i = 0; i < item.size; i++) {
+                  width += styles.item.width;
+                  views.push(<View style={[styles.item, {backgroundColor: item.color}]}>
+                    <Text style={styles.itemText}>{item.title + i}</Text>
+                  </View>);
+                }
+              }else{
+                views.push(<View style={{width: item.width,height: 200,backgroundColor: '#00ff00'}}>
+                  <Text style={styles.itemText}>{item.title}</Text>
+                </View>);
+              }
+
+              return <View style={{flexDirection: 'row'}}>{views}</View>;
+            }
+            }
+            onScroll={Animated.event(
+                [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+                { useNativeDriver: true }
+            )}
+            // contentContainerStyle={styles.listContent}
+        />
+
+       {/* <View style={styles.indicatorContainer}>
+          {testData.map((_, index) => {
+            const opacity = scrollX.interpolate({
+              inputRange: [
+                (index - 1) * itemWidth,
+                index * itemWidth,
+                (index + 1) * itemWidth,
+              ],
+              outputRange: [0.3, 1, 0.3],
+              extrapolate: 'clamp',
+            });
+
+            return (
+                <Animated.View
+                    key={index}
+                    style={[styles.indicator, { opacity }]}
+                />
+            );
+          })}
+        </View>*/}
+      </View>
   );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    backgroundColor: '#f5f5f5',
   },
-  sectionTitle: {
+  title: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  listContent: {
+    paddingHorizontal: (Dimensions.get('window').width * 0.1) / 2, // 居中显示
   },
-  highlight: {
-    fontWeight: '700',
+  item: {
+    width: Dimensions.get('window').width * 0.5,
+    height: 200,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // marginHorizontal: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  item2: {
+    width: 0,
+    height: 200,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // marginHorizontal: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  itemText: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  indicatorContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  indicator: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#333',
+    marginHorizontal: 4,
   },
 });
 
