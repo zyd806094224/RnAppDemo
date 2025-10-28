e7c0632419e96751fe547b5c70f912b866081206
zhaoyudong <zhaoyudong@staff.hexun.com>
2025-10-28 08:24:32 +0800

    git忽略文件修改、规范化代码

--- src/navigators/AppNavigator.tsx
+++ src/navigators/AppNavigator.tsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from "../screens/HomeScreen";
-import HorizontalListPage from "../screens/HorizontalListPage";
-import MarqueeHorizontalPage from "../screens/MarqueeHorizontalPage";
+import HomeScreen from '../screens/HomeScreen';
+import HorizontalListPage from '../screens/HorizontalListPage';
+import MarqueeHorizontalPage from '../screens/MarqueeHorizontalPage';
 
 export type RootStackParamList = {
     Home: undefined;
